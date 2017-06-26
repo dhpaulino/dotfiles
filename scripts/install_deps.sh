28b9cadc260afb3834639b0bab606391d2706c66
Davisson Paulino <dhpaulino@gmail.com>
2017-06-26 11:58:56 -0300

    adicionado lock e outras coisinhas

--- scripts/install_deps.sh
+++ scripts/install_deps.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+apt-get install i3 i3blocks
 #font-awesome
-sudo apt-get install fonts-font-awesome
+apt-get install fonts-font-awesome
+
+#locker
+sudo apt-get install xautolock
+apt-get install scrot
 
 #battery
-sudo apt-get install acpi
+apt-get install acpi
 wget https://github.com/acrisci/playerctl/releases/download/v0.5.0/playerctl-0.5.0_amd64.deb -P ~/Downloads/
 
-dpkg - i ~/Downloads/playerctl-0.5.0_amd64.deb
+
+dpkg -i ~/Downloads/playerctl-0.5.0_amd64.deb
