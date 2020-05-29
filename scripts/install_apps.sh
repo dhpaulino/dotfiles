9266c6fc399ced8293f49f6686afb79f67c0e6a9
Davison Paulino <dhpaulino@gmail.com>
2020-05-29 13:52:26 -0300

    fix: install updated visual code

--- scripts/install_apps.sh
+++ scripts/install_apps.sh
@@ -1,16 +1,11 @@
 #/bin/bash
 
-#spotify
-#apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886 0DF731E45CE24F27EEEB1450EFDC8610341D9410
+apt update
 
-echo deb http://repository.spotify.com stable non-free > /etc/apt/sources.list.d/spotify.list
-
-apt-get update
-
-wget http://ftp.br.debian.org/debian/pool/main/o/openssl/libssl1.0.0_1.0.1t-1+deb8u6_amd64.deb -O /tmp/libssl.deb 
-
-apt install /tmp/libssl.deb -y
-apt-get install spotify-client -y --allow-unauthenticated
+# spotify
+curl -sS https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add - 
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt-get update && sudo apt-get install spotify-client
 
 apt install vim -y
 apt install firefox-esr -y
@@ -19,6 +14,5 @@ apt install evince
 
 
 #visual studio code
-wget https://az764295.vo.msecnd.net/stable/b813d12980308015bcd2b3a2f6efa5c810c33ba5/code_1.17.2-1508162334_amd64.deb -O /tmp/visual_code.deb
-
-apt install /tmp/visual_code.deb -y 
+apt install snapd # Contained apps store
+snap install code --classic
